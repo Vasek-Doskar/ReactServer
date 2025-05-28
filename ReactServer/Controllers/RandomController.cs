79d7c406dc6c0fa9d9d90ba8493db74a4fb710b9
doskar <doskar@spsjc.cz>
2025-05-28 09:23:49 +0200

    Add project files.

---
+++ ReactServer/Controllers/RandomController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReactServer.Models;
+
+namespace ReactServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetRandom([FromBody] Randomator randomator)
+        {
+            if(randomator is not null)
+            {
+                return Ok(randomator.Random);
+            }
+            return BadRequest("Chyba");
+        }
+    }
+}
---
+++ ReactServer/Models/Randomator.cs
@@ -0,0 +1,9 @@
+﻿namespace ReactServer.Models
+{
+    public class Randomator
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Random => new Random().Next(Min, Max + 1);
+    }
+}
